d532447a171a8e5707db104fbcac773c3a078707
CR-MX <ciat117@gmail.com>
2023-03-25 18:39:50 -0600

    productos

---
+++ app/Producto.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Producto
+ *
+ * @property $id
+ * @property $seccion_id
+ * @property $clave
+ * @property $nombre
+ * @property $imagen
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property Seccione $seccione
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Producto extends Model
+{
+    
+    static $rules = [
+		'clave' => 'required',
+		'nombre' => 'required',
+		'imagen' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['seccion_id','clave','nombre','imagen'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function seccione()
+    {
+        return $this->hasOne('App\Seccione', 'id', 'seccion_id');
+    }
+    
+
+}
---
+++ database/migrations/2023_03_26_003440_producto.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Producto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('seccion_id')->nullable();
+            $table->foreign('seccion_id')
+                ->references('id')->on('secciones');
+            $table->integer('clave');
+            $table->text('nombre');
+            $table->longText('imagen');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
