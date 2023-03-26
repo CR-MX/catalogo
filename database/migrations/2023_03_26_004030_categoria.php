aa367b03c61cb957b75bc9a46e8d5ef25da3a4d7
CR-MX <ciat117@gmail.com>
2023-03-25 18:45:07 -0600

    categorias

--- database/migrations/2023_03_26_004030_categoria.php
+++ database/migrations/2023_03_26_004030_categoria.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Seccion extends Migration
+class Categoria extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class Seccion extends Migration
      */
     public function up()
     {
-        Schema::create('secciones', function (Blueprint $table) {
+        Schema::create('categorias', function (Blueprint $table) {
             $table->id();
             $table->text('nombre');
             $table->timestamps();
@@ -27,6 +27,6 @@ class Seccion extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('secciones');
+        Schema::dropIfExists('categorias');
     }
 }
