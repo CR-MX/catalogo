907fe1fef9e68e6496b014f1c639dc407b1c9f15
CR-MX <ciat117@gmail.com>
2023-03-25 18:34:29 -0600

    seccion

---
+++ database/migrations/2023_03_26_002956_seccion.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Seccion extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('secciones', function (Blueprint $table) {
+            $table->id();
+            $table->text('nombre');
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('secciones');
+    }
+}
