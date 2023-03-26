8e305468377b4dfe7572b3f2f7b80c1421c1eecc
CR-MX <ciat117@gmail.com>
2023-03-25 18:26:33 -0600

    Revert "categoria"
    This reverts commit 5b2ef36e09c55fb3b2d1182899fb5182aea04d33.

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::resource('productos', App\Http\Controllers\ProductoController::class);
-Route::resource('categorias', App\Http\Controllers\CategoriaController::class);
 Route::get('/', function () {
     return view('welcome');
 });
